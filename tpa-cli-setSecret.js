fc5684481402f219eae1ed52adba2d973b05418e
Daniel Cornelison <itsklastic@gmail.com>
2017-03-19 06:03:01 -0500

    added code

---
+++ tpa-cli-setSecret.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+const program = require('commander');
+const json = require('json-update');
+
+program.parse(process.argv);
+
+if (!program.args) {
+  console.error('Consumer Secret Required');
+  process.exit(1);
+}
+
+json.update('config.json',{consumer_secret:program.args})
+.then(function(dat) { 
+  console.log('Your Secret has been updated.') 
+});
