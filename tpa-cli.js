50a5334cabe7d64cae9479dfef9deb1c449525d2
Daniel Cornelison <itsklastic@gmail.com>
2017-03-19 16:51:01 -0500

    changed text

--- tpa-cli.js
+++ tpa-cli.js
@@ -3,10 +3,11 @@
 'use strict';
 
 const program = require('commander');
+// module.exports.program = program;
 
 program
 .version('1.0.0')
 .command('setKey [key]','Sets the Consumer Key').alias('sK')
 .command('setSecret [secret]','Sets the Consumer Secret').alias('sS')
-.command('auth','Generates an OAuth URL and waits for the PIN to be entered to authorize the account.')  
+.command('auth','Generates an OAuth URL and waits for the PIN to be entered to authorize the account.').alias('a')  
 .parse(process.argv);
