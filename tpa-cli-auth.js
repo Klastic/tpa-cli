728a2ce5ae013ca19e27dd78dbb020c19f950000
Daniel Cornelison <itsklastic@gmail.com>
2017-03-19 15:12:12 -0500

    added color to prompt

--- tpa-cli-auth.js
+++ tpa-cli-auth.js
@@ -1,11 +1,14 @@
+#!/usr/bin/env node
+
 const TwitterPinAuth = require('twitter-pin-auth');
 const config = require('./config');
 const twitterPinAuth = new TwitterPinAuth(config.consumer_key, config.consumer_secret);
 const prompt = require('prompt');
 const program = require('commander');
+const chalk = require('chalk');
 
 program.parse(process.argv);
-if(!config.consumer_key[0] || !config.consumer_secret[0]) return console.log('You are missing your Key or Secret. Use setKey or setSecret command to add them.');
+if(!config.consumer_key[0] || !config.consumer_secret[0]) return console.log(chalk.styles.red.open + 'You are missing your Key or Secret. Use the'+ chalk.styles.red.close + chalk.styles.cyan.open +'setKey '+ chalk.styles.cyan.close + chalk.styles.red.open +'or'+ chalk.styles.red.close + chalk.styles.cyan.open +' setSecret '+ chalk.styles.cyan.close + chalk.styles.red.open +' command to add them.' + chalk.styles.red.close);
 
 twitterPinAuth.requestAuthUrl()
 .then(function(url) {
@@ -19,7 +22,7 @@ twitterPinAuth.requestAuthUrl()
             if(err) {
                 return console.error(err);
             }
-            console.log('Your Access Token Key: '+data.accessTokenKey);
+            console.log(chalk.styles.yellow.open + 'Your Access Token Key: ' + chalk.styles.yellow.close + chalk.styles.green.open + data.accessTokenKey + chalk.styles.green.close);
             console.log('Your Access Token Secret: '+data.accessTokenSecret);
         });
     });
